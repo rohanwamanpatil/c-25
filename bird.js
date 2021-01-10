18d8dde21ebcef1d10d5950cb84d0cbf53499858
rohanwamanpatil <74587373+rohanwamanpatil@users.noreply.github.com>
2021-01-10 16:06:03 +0530

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,47 @@
+class  Bird{
+    //properties
+constructor(x,y){
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, 100,100, options);
+    this.width = 50;
+    this.height = 50;
+    
+    World.add(world, this.body);
+}
+//functions
+display(){
+    var pos =this.body.position;
+    pos.x=mouseX;
+    pos.y=mouseY;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("red");
+    stroke ("yellow");
+    strokeWeight(5);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
